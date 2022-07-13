d643d2b87746d90a94c241e5d228a37ebc60fd31
Thainá Monteiro <96400582+Thaina-Monteiro@users.noreply.github.com>
2022-07-12 23:21:33 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="App">Prática React 02 - Component de Class</h1>
+      </div>
+    );
+  }
+}
